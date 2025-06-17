449253193fdcbc5c7d83c7e4b6a5e3369649f452
xuewanyun <xuewanyun@yzw.cn>
2025-06-17 14:14:40 +0800

    feat: solidity

--- solidity_base/task1/VotingContract.sol
+++ solidity_base/task1/VotingContract.sol
@@ -8,15 +8,26 @@
 
 pragma solidity ^0.8.0;
 contract VotingContract {
+    // 候选人票数
     mapping(string => uint256) votes;
-
+    // 候选人列表
+    string[] usersList;
+    // 投票允许用户投票给某个候选人
     function vote(string memory user) public {
         votes[user]++;
     }
-
+    // 获取某个候选人的票数
     function getVotes(string memory user) public view returns (uint256) {
         return votes[user];
     }
-
-    function resetVotes() public {}
+    // 添加候选人列表
+    function addUsee(string memory user) public {
+        usersList.push(user);
+    }
+    // 重置所有候选人的得票数
+    function resetVotes() public {
+        for (uint256 i = 0; i < usersList.length; i++) {
+            votes[usersList[i]] = 0;
+        }
+    }
 }
